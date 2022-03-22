5a137ff314bd2646f926ff9eed1da384d19500ab
p-centenera <100157069+p-centenera@users.noreply.github.com>
2022-03-22 18:30:34 +0100

    Version 1

--- src/Ppal/AnalisisEmpiricoOrdenacionYBusqueda.java
+++ src/Ppal/AnalisisEmpiricoOrdenacionYBusqueda.java
@@ -6,21 +6,20 @@ import java.util.function.Function;
 import java.util.ArrayList;
 import java.util.stream.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class AnalisisEmpiricoOrdenacionYBusqueda {
-    public static void swap( long A, long B){
-        long Aux=A;
-        A=B;
-        B=Aux;
 
-    }
     public static void main(String[] args) {
     Function<long[], long[]>  BubbleSort = L->
     {
         for (int i = 1; i < L.length; i++)
-            for (int j = L.length - 1; j > i; j--)
-                if (L[j - 1] > L[j])
-                    swap(L[j - 1] , L[j]);
+            for (int j = L.length - 1; j >= i; j--)
+                if (L[j - 1] > L[j]) {
+                    long Aux=L[j];
+                    L[j]=L[j-1];
+                    L[j-1]=Aux;
+                }
         return L;
     };
 
@@ -28,29 +27,38 @@ public class AnalisisEmpiricoOrdenacionYBusqueda {
         long[] [] JuegoPruebasAleatorio =new long[6][];
         long [] []  JuegoPruebasOrdenado =new long[6][];
         long [] []  JuegoPruebasInverso =new long[6][];
-        Long tam= new Long(100);
+        long tam= 100;
 
         Random random =new Random();
 
         for (int i=0;i<6;i++){
-            JuegoPruebasAleatorio[i]= random.longs().
+            JuegoPruebasAleatorio[i]= random.longs().map(Math::abs).
                     limit(tam).toArray();
             JuegoPruebasOrdenado[i]=Stream.iterate( (long) 0, x -> x + 1).limit(tam).mapToLong(x->x).toArray();
             JuegoPruebasInverso[i]=Stream.iterate(tam, x -> x - 1).limit(tam).mapToLong(x->x).toArray();
             tam*=10;
         }
         try{
-            tam=Long.valueOf(100);
+            tam=100;
             FileWriter fichero = new FileWriter("tiempos.txt");
             PrintWriter out = new PrintWriter(fichero);
-            for (int i=0;i<6;i++){
+            long[] resultado;
+            for (int i=0;i<2;i++){
                 out.print(tam+"\t");
-                Analizador.analiza(JuegoPruebasAleatorio[i],BubbleSort,out);
-                Analizador.analiza(JuegoPruebasOrdenado[i],BubbleSort,out);
-                Analizador.analiza(JuegoPruebasInverso[i],BubbleSort,out);
-                out.println();
                 System.out.println("tamaño: "+tam);
-                tam*=10;
+                resultado=Analizador.analiza(JuegoPruebasAleatorio[i],BubbleSort,out);
+                System.out.println("aleatorio: "+Arrays.toString(JuegoPruebasAleatorio[i]));
+                System.out.println("ordenado: "+Arrays.toString(resultado));
+
+                resultado=Analizador.analiza(JuegoPruebasOrdenado[i],BubbleSort,out);
+                System.out.println("ya ordenado: "+Arrays.toString(JuegoPruebasOrdenado[i]));
+                System.out.println("ordenado: "+Arrays.toString(resultado));
+
+                resultado=Analizador.analiza(JuegoPruebasInverso[i],BubbleSort,out);
+                System.out.println("inverso: "+Arrays.toString(JuegoPruebasInverso[i]));
+                System.out.println("ordenado: "+Arrays.toString(resultado));
+                out.println();
+                 tam*=10;
             }
             out.close();
 
