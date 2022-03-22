6ddba739f85809a8390d9ccc8d7a2a974fa7fdc4
p-centenera <100157069+p-centenera@users.noreply.github.com>
2022-03-22 17:44:48 +0100

    Version 1

--- src/Modelo/Analizador.java
+++ src/Modelo/Analizador.java
@@ -1,14 +1,14 @@
 package Modelo;
 import java.io.*;
 import java.util.function.Function;
+
 public class Analizador {
-    public static void analiza(long[] data, Function F,  PrintWriter out) throws IOException{
-        long[] aux =new long[data.length];
-        for(int i=0;i<aux.length;i++)
-            aux[i]=data[i];
-        Long inicio=System.nanoTime();
+    public static long [] analiza(long[] data, Function <long[],long[]> F,  PrintWriter out) {
+        long[] aux = data.clone();
+        long inicio=System.nanoTime();
         F.apply(aux);
-        Long duracion=System.nanoTime()-inicio;
+        long duracion=System.nanoTime()-inicio;
         out.print(duracion+"\t");
+        return aux;
     }
 }
